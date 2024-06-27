76b6bdc4cf7df1ee73de17364df8f150feddfd37
IrinaEu <irina.euphoriya@gmail.com>
2024-06-26 21:34:51 -0400

    Pushing six times

--- src/test/java/org/example/PushingTwo.java
+++ src/test/java/org/example/PushingTwo.java
@@ -3,5 +3,6 @@ package org.example;
 public class PushingTwo {
     public static void main(String[] args) {
         System.out.println("booo");
+        System.out.println("Comig soon");
     }
 }
